da8762c7481d0efb1921d0e4122bb555bc520fc0
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-06-14 21:32:26 -0500

    avance-1

---
+++ assets/js/components/header.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const Header = (update) => {
+  const header = $('<header></header>');
+  const container = $('<div class="container"></div>');
+  const row = $('<div class="row"></div>');
+  const col = $('<div class="col m12"></div>');
+  const title = $('<h1 class="center-align style-title">Pokédex</h1>');
+
+  header.append(container);
+  container.append(row);
+  row.append(col);
+  col.append(title);
+
+  return header;
+}
